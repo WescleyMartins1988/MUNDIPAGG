472cb5a8f03a6c9ff1e5e555748967ab975d8091
WescleyMartins1988 <wescleymartins02@gmail.com>
2017-12-02 16:41:44 -0200

    Update

--- js/controllers/HomeCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module("MundiPaggRepos").controller('homeCtrl', function($scope){
-	$scope.title = "Desafio Mundipagg";
-	$scope.description = "Esta aplicação foi baseada em um desafio feito pela empresa Mundipagg, o objetivo é desenvolver uma aplicação que deverá integrar com a API-V3 do Github e listar alguns dados do Github da Mundipagg."
-	$scope.info = "Clique em Listar para ver os dados do Github da Mundipagg."
-	$scope.description2 = "A aplicação deve ser desenvolvida em HTML, CSS e JS, sendo livre o uso de Frameworks, listar os Repositórios e mostrar a quantidade de Stars, Forks e Contributers por projeto."
-});
