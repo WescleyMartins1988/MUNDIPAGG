415df394cf47e1675ff91588a8e15f4dd36ded2e
WescleyMartins1988 <wescleymartins02@gmail.com>
2017-12-02 17:17:52 -0200

    Update

--- app/js/directives/NavbarDirective.js
+++
@@ -1,6 +0,0 @@
-angular.module("MundiPaggRepos").directive("uinavbar", function(){
-	return {
-		templateUrl: "view/NavBar.html",
-		replace: true
-	}
-})
--- app/js/filters/titleFilter.js
+++
@@ -1,7 +0,0 @@
-angular.module("MundiPaggRepos").filter("titleFilter", function() {
-	return function(input){
-		var reposFilter = input.replace(/_/g, "-");
-		return reposFilter;
-	}
-	return input;
-});
